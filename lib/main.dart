8f1e40f7fa7100b5da1217f18c0eff5c47b560d5
hasanhlc <147191451+hasanhlc@users.noreply.github.com>
2024-12-25 20:58:13 +0300

    last

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pokemon App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -28,15 +29,35 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   List pokemonList = [];
   final List<Color> colors = List.generate(100,
       (index) => Colors.primaries[Random().nextInt(Colors.primaries.length)]);
+
+  late AnimationController _animationController;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
     fetchPokemonData();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   Future<void> fetchPokemonData() async {
@@ -52,32 +73,106 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Future<void> fetchPokemonDetails(String url) async {
+  Future<void> fetchPokemonDetails(String url, Color cardColor) async {
     var dio = Dio();
     try {
       var response = await dio.get(url);
-      showDialog(
+      _animationController.reset();
+      _animationController.forward();
+      showGeneralDialog(
         context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text(response.data['name'].toString().toUpperCase()),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.network(response.data['sprites']['front_default']),
-                Text('Height: ${response.data['height']}'),
-                Text('Weight: ${response.data['weight']}'),
-                Text('Base Experience: ${response.data['base_experience']}'),
-              ],
-            ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Kapat'),
+        barrierDismissible: true,
+        barrierLabel:
+            MaterialLocalizations.of(context).modalBarrierDismissLabel,
+        transitionDuration: const Duration(milliseconds: 500),
+        pageBuilder: (context, anim1, anim2) {
+          return ScaleTransition(
+            scale: _animation,
+            child: Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
-            ],
+              child: Container(
+                width: 300,
+                padding: const EdgeInsets.all(16.0),
+                color: cardColor,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Divider(thickness: 2, color: Colors.black),
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        response.data['name'].toString().toUpperCase(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const Divider(thickness: 2, color: Colors.black),
+                    const SizedBox(height: 10),
+                    Image.network(
+                      response.data['sprites']['front_default'],
+                      height: 150,
+                      fit: BoxFit.contain,
+                    ),
+                    const SizedBox(height: 10),
+                    const Divider(thickness: 2, color: Colors.black),
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        'Height: ${response.data['height']}',
+                        style:
+                            const TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        'Weight: ${response.data['weight']}',
+                        style:
+                            const TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        'Base Experience: ${response.data['base_experience']}',
+                        style:
+                            const TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Divider(thickness: 2, color: Colors.black),
+                    Align(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          _animationController
+                              .reverse()
+                              .then((value) => Navigator.of(context).pop());
+                        },
+                        child: const Text('Kapat'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+        transitionBuilder: (context, anim1, anim2, child) {
+          return ScaleTransition(
+            scale: Tween<double>(begin: 0.5, end: 1.0).animate(CurvedAnimation(
+              parent: anim1,
+              curve: Curves.easeInOut,
+            )),
+            child: child,
           );
         },
       );
@@ -90,7 +185,14 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pokemonlar'),
+        title: const Text(
+          'Pokemons',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: pokemonList.isEmpty
           ? const Center(child: CircularProgressIndicator())
@@ -109,7 +211,8 @@ class _HomePageState extends State<HomePage> {
                 final imageUrl =
                     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png';
                 return GestureDetector(
-                  onTap: () => fetchPokemonDetails(pokemon['url']),
+                  onTap: () =>
+                      fetchPokemonDetails(pokemon['url'], colors[index]),
                   child: Card(
                     color: colors[index],
                     elevation: 4,
@@ -123,7 +226,7 @@ class _HomePageState extends State<HomePage> {
                           fit: BoxFit.cover,
                         ),
                         const SizedBox(height: 10),
-                        Divider(thickness: 2, color: Colors.black),
+                        const Divider(thickness: 2, color: Colors.black),
                         Text(
                           pokemon['name'].toString().toUpperCase(),
                           style: const TextStyle(
